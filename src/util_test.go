01bad67281f11e24e2cdf23cd47635be945f4397
Stephen Kelly <stevekm@users.noreply.github.com>
2022-06-01 19:35:38 -0400

    add test case for too many files open

--- src/util_test.go
+++ src/util_test.go
@@ -6,31 +6,52 @@ import (
 	"testing"
 	"os"
 	"path/filepath"
+	"strconv"
 	"github.com/google/go-cmp/cmp"
 )
 
-
-func TestUtil(t *testing.T){
-	tempdir := t.TempDir() // automatically gets cleaned up when all tests end
-	tempfile1, err := os.CreateTemp(tempdir, "file1.") // *os.File
+// create a temp file in a dir and write something to its contents
+func createTempFile(tempdir string, filename string, contents string) *os.File {
+	tempfile, err := os.CreateTemp(tempdir, filename)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer tempfile1.Close()
+	// defer tempfile.Close()
 
 	// write to the file
-	nbytesWritten, err := tempfile1.WriteString("writes\n")
-	if err != nil {
-		fmt.Println(nbytesWritten)
-		log.Fatal(err)
+	if contents != "" {
+		nbytesWritten, err := tempfile.WriteString(contents)
+		if err != nil {
+			fmt.Println(nbytesWritten)
+			log.Fatal(err)
+		}
+
+		// need to reset the cursor after writing
+		i, err := tempfile.Seek(0, 0)
+		if err != nil {
+			fmt.Println("Error", err, i)
+			log.Fatal(err)
+		}
 	}
 
-	// need to reset the cursor after writing
-	i, err := tempfile1.Seek(0, 0)
-	if err != nil {
-		fmt.Println("Error", err, i)
-		log.Fatal(err)
-	}
+	return tempfile
+}
+
+// generate a list of numbers
+func makeRange(min, max int) []int {
+	a := make([]int, max-min+1)
+	for i := range a {
+			a[i] = min + i
+		}
+	return a
+}
+
+
+
+func TestUtil(t *testing.T){
+	// setup test dirs & files
+	tempdir := t.TempDir() // automatically gets cleaned up when all tests end
+	tempfile1 := createTempFile(tempdir, "f.", "writes\n")
 
 	t.Run("Get md5 hash", func(t *testing.T) {
 		got := getFileMD5(tempfile1)
@@ -49,7 +70,7 @@ func TestUtil(t *testing.T){
 
 
 func TestFinder(t *testing.T) {
-	// set up temp dirs for files
+	// set up temp dirs for tests
 	tempdir := t.TempDir() // automatically gets cleaned up when all tests end
 	subdir1 := filepath.Join(tempdir, "subdir.1")
 	err := os.MkdirAll(subdir1, os.ModePerm)
@@ -72,22 +93,14 @@ func TestFinder(t *testing.T) {
 	// tempfile2 and tempfile3 have different names but same contents (empty)
 	// tempfile3 and tempfile4 have same names and same contents (empty) but different directories
 	// tempfile5 is in the subdir to skip and has same size as tempfile2, tempfile3
-	tempfile1, err := os.CreateTemp(subdir1, "file1.") // *os.File
-	if err != nil {
-		log.Fatal(err)
-	}
+
+	tempfile1 := createTempFile(subdir1, "file1.", "writes\n")
 	defer tempfile1.Close()
 
-	tempfile2, err := os.CreateTemp(subdir2, "file2.")
-	if err != nil {
-		log.Fatal(err)
-	}
+	tempfile2 := createTempFile(subdir2, "file2.", "")
 	defer tempfile2.Close()
 
-	tempfile3, err := os.CreateTemp(tempdir, "file3.")
-	if err != nil {
-		log.Fatal(err)
-	}
+	tempfile3 := createTempFile(tempdir, "file3.", "")
 	defer tempfile3.Close()
 
 	fi, err := tempfile3.Stat()
@@ -103,23 +116,14 @@ func TestFinder(t *testing.T) {
 	}
 	defer tempfile4.Close()
 
-	tempfile5, err := os.CreateTemp(subdir3, "file5.")
-	if err != nil {
-		log.Fatal(err)
-	}
+	tempfile5 := createTempFile(subdir3, "file5.", "")
 	defer tempfile5.Close()
-
-	// write to the file
-	nbytesWritten, err := tempfile1.WriteString("writes\n")
-	if err != nil {
-		fmt.Println(nbytesWritten)
-		log.Fatal(err)
-	}
 
 	t.Run("Test find dupes", func(t *testing.T) {
 		var skipDirs = []string{subdir3}
 		got := FindDupes(tempdir, skipDirs)
 		// fmt.Printf("Duplicate file sizes: %v\n", got)
+		// fmt.Println(len(got))
 		want := map[string][]string{
 			"d41d8cd98f00b204e9800998ecf8427e": []string{
 				tempfile3.Name(),
@@ -127,7 +131,7 @@ func TestFinder(t *testing.T) {
 				tempfile4.Name(),
 			},
 		}
-
+		// NOTE: might need to revise this test to not depend on order of items in the list!
 		if diff := cmp.Diff(want, got); diff != "" {
 			t.Errorf("got vs want mismatch (-want +got):\n%s", diff)
 		}
@@ -142,4 +146,39 @@ func TestFinder(t *testing.T) {
 		}
 
 	})
+}
+
+
+
+func TestTooManyFiles(t *testing.T){
+	// setup test dirs & files
+	tempdir := t.TempDir() // automatically gets cleaned up when all tests end
+	// make a large number of temp files, each with different contents
+	ints := makeRange(0, 20000)
+	for i := range ints {
+		i_str := strconv.Itoa(i)
+		t := createTempFile(tempdir, i_str, i_str)
+		t.Close()
+	}
+	// create two temp files with the same contents
+	tempfile1 := createTempFile(tempdir, "f.", "foo")
+	defer tempfile1.Close()
+	tempfile2 := createTempFile(tempdir, "f2.", "foo")
+	defer tempfile2.Close()
+
+	t.Run("Find dupes without exceeding the limit on number of open files", func(t *testing.T) {
+
+		var skipDirs = []string{}
+		got := FindDupes(tempdir, skipDirs)
+		want := map[string][]string{
+			"acbd18db4cc2f85cedef654fccc4a4d8": []string{
+				tempfile1.Name(),
+				tempfile2.Name(),
+			},
+		}
+		if diff := cmp.Diff(want, got); diff != "" {
+			t.Errorf("got vs want mismatch (-want +got):\n%s", diff)
+		}
+	})
+
 }
