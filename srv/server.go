025f3dda1fbe4baab1f6af5612a56257946e7fdf
Stephen Kelly <stevekm@users.noreply.github.com>
2022-06-17 09:28:18 -0400

    update server

--- srv/server.go
+++ srv/server.go
@@ -11,14 +11,7 @@ import (
 // $ curl "http://localhost:1000/?p=/path/to/dir"
 func rootHandler(writer http.ResponseWriter, request *http.Request){
 	query := request.URL.Query()
-
-// get query params
-	if pathList, ok := query["p"]; ok { // path
-		path := pathList[0]
-		if path != "" {
-			log.Printf("got path from query: %v\n", path)
-		}
-	}
+	log.Printf("rootHandler got query: %v\n", query)
 
 // send response
 	json.NewEncoder(writer).Encode("foo")
@@ -31,18 +24,12 @@ func findDupersHandler(writer http.ResponseWriter, request *http.Request){
 
 	findConfig := finder.FindConfig{}
 	hashConfig := finder.HashConfig{}
-	// formatConfig := finder.FormatConfig{}
 
 	if pathList, ok := query["p"]; ok { // path
 		path := pathList[0]
-		if path != "" {
+		if path != "" { // map[p:[/path/to/dir]] ; default to empty string
 			log.Printf("got path from query: %v\n", path)
-
 			dupes := finder.FindDupes(path, findConfig, hashConfig)
-			// for _, entries := range dupes {
-			// 	format := finder.DupesFormatter(entries, formatConfig)
-			// 	fmt.Printf("%s", format) // format has newline embedded at the end
-			// }
 			json.NewEncoder(writer).Encode(dupes)
 		}
 	}
@@ -50,13 +37,15 @@ func findDupersHandler(writer http.ResponseWriter, request *http.Request){
 
 }
 
-func handleRequests(){
+func handleRequests(port string){
 	http.HandleFunc("/", rootHandler)
 	http.HandleFunc("/v0/", findDupersHandler)
-	log.Fatal(http.ListenAndServe(":1000", nil))
+	log.Fatal(http.ListenAndServe(":" + port, nil))
 }
 
 func main(){
-	log.Println("hellooo world")
-	handleRequests()
+	port := "1000"
+	url := "http://localhost"
+	log.Printf("Running on %v:%v\n", url, port)
+	handleRequests(port)
 }
